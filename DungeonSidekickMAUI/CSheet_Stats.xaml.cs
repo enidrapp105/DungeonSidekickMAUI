aefc540a96dcd4b7fd290d7958917cea5051da45
Brendon Williams <brendon.williams@oit.edu>
2024-01-21 00:48:02 -0800

    Working redoing the updates, one .xaml at a time

---
+++ DungeonSidekickMAUI/CSheet_Stats.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DungeonSidekickMAUI
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CSheet_Stats : ContentPage
+	{
+		public CSheet_Stats ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
