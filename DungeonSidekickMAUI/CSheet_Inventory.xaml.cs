4310cfae963c18054acba0b18f50721491a8b5c3
Brendon Williams <brendon.williams@oit.edu>
2024-01-21 20:32:18 -0800

    Moved code from old iteration, working on .xaml aspect next

--- DungeonSidekickMAUI/CSheet_Inventory.xaml.cs
+++ DungeonSidekickMAUI/CSheet_Inventory.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,45 @@ namespace DungeonSidekickMAUI
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CSheet_Inventory : ContentPage
     {
-        public CSheet_Inventory()
+        private CharacterSheet CharacterSheetcurrent;
+        public CSheet_Inventory(CharacterSheet sheet)
         {
+            CharacterSheetcurrent = sheet;
             InitializeComponent();
+            Inventory.Text = CharacterSheetcurrent.characterclass.ClassDesc;
+        }
+        private void SubmitStats(object sender, EventArgs e)
+        {
+            string connectionString = "server=satou.cset.oit.edu, 5433; database=harrow; UID=harrow; password=5HuHsW&BYmiF*6";
+
+            string query = "INSERT INTO dbo.CharacterSheet" +
+                "(CharacterName,PlayerName,Race,Class,Background,Alignment,PersonalityTraits,Ideals,Bonds,Flaws," +
+                "FeaturesTraits,Equipment,Proficiencies,Attacks,Spells,Strength,Dexterity,Constitution,Intelligence,Wisdom,Charisma) VALUES" +
+                "(@CharacterName,@PlayerName,@Race,@Class,@Background,@Alignment,@PersonalityTraits,@Ideals,@Bonds," +
+                "@Flaws,@FeaturesTraits,@Equipment,@Proficiencies,@Attacks,@Spells,@Strength,@Dexterity,@Constitution,@Intelligence,@Wisdom,@Charisma);";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    if (conn.State == System.Data.ConnectionState.Open)
+                    {
+                        using (SqlCommand cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = query;
+                            cmd.Parameters.AddWithValue("@Equipment", Inventory.Text);
+                            cmd.Parameters.AddWithValue("@Proficiencies", Proficiencies.Text);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    conn.Close();
+                }
+            }
+            catch (Exception eSql)
+            {
+                Debug.WriteLine("Exception: " + eSql.Message);
+            }
         }
     }
+}
 }
