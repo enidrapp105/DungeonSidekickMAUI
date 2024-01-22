8faf96b7921d65fb30c2ccb5586355310db401b9
Brendon Williams <brendon.williams@oit.edu>
2024-01-21 20:29:50 -0800

    creating the new class so that I can pull code from earlier iteration

---
+++ DungeonSidekickMAUI/CSheet_Inventory.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DungeonSidekickMAUI
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CSheet_Inventory : ContentPage
+    {
+        public CSheet_Inventory()
+        {
+            InitializeComponent();
+        }
+    }
+}
