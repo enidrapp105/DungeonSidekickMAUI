21b678fd6823cb77f2886ae7a889bc3d37f533ab
Brendon Williams <brendon.williams@oit.edu>
2024-02-04 12:10:59 -0800

    Very very basic decoration within a landing page. Also removed titles from the other xamls that still had them

--- DungeonSidekickMAUI/LandingPage.xaml.cs
+++ DungeonSidekickMAUI/LandingPage.xaml.cs
@@ -3,8 +3,37 @@ namespace DungeonSidekickMAUI;
 public partial class LandingPage : ContentPage
 {
     CharacterSheet currentcharacterSheet;
-    public LandingPage(CharacterSheet currentCharacterSheet)
+    public LandingPage(CharacterSheet passedCharacterSheet)
 	{
+        currentcharacterSheet = passedCharacterSheet;
 		InitializeComponent();
+
+        if (currentcharacterSheet != null ) 
+        {
+            LoadCharacterSheetPage(currentcharacterSheet);
+        }
+        else
+        {
+            DisplayAlert("Your character sheet didn't convert correctly", "Let's retry making one", "Ok");
+            Navigation.PushAsync(new MainPage());
+        }
 	}
+
+    private void LoadCharacterSheetPage(CharacterSheet characterSheet)
+    {
+        int strmod = (int.Parse(characterSheet.strength) - 10) / 2; //Doing math externally
+        int dexmod = (int.Parse(characterSheet.dexterity) - 10) / 2; //Using int to drop decimal / round down
+        int constmod = (int.Parse(characterSheet.constitution) - 10) / 2;
+        int intmod = (int.Parse(characterSheet.intelligence) - 10) / 2;
+        int wismod = (int.Parse(characterSheet.wisdom) - 10) / 2;
+        int charmod = (int.Parse(characterSheet.charisma) - 10) / 2;
+        
+        
+        Str_Mod.Text = strmod.ToString();
+        Dex_Mod.Text = dexmod.ToString();
+        Const_Mod.Text = constmod.ToString();
+        Int_Mod.Text = intmod.ToString();
+        Wis_Mod.Text = wismod.ToString();
+        Char_Mod.Text = charmod.ToString();
+    }
 }
