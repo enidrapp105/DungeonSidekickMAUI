0d69516c312c3792e6f3787903659fb9c7517d36
Wolfl <Wolfl@MY3RDPC>
2024-02-04 20:08:11 -0800

    made the diceroll methods its own class to be used in the whole app

---
+++ DungeonSidekickMAUI/DiceRoll.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Maui.Animations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonSidekickMAUI
+{
+    class DiceRoll
+    {
+        Random random;
+
+        public DiceRoll()
+        {
+            long ticks = DateTime.Now.Ticks;
+            this.random = new Random((int)ticks);
+        }
+
+        public int Roll(string input)
+        {
+            long ticks = DateTime.Now.Ticks;
+            this.random = new Random((int)ticks);
+            int sum = ParseAndRoll(input);
+            return sum;
+        }
+
+        /*
+         * Function: RollDice
+         * Author: Kenny Rapp
+         * Purpose: to roll dice and return the rolled sum
+         * last Modified: 2/4/2024 6:21pm By Kenny Rapp
+         */
+        private int RollDice(int numberOfDice, int numberOfFaces)
+        {
+            int totalSum = 0;
+            if (numberOfFaces > 0)
+            {
+                for (int i = 0; i < numberOfDice; i++)
+                {
+                    int rollResult = random.Next(1, numberOfFaces + 1);
+                    totalSum += rollResult;
+                }
+            }
+            return totalSum;
+        }
+
+        /*
+         * Function: ParseAndRoll
+         * Author: Kenny Rapp
+         * Purpose: to parse the input string and return the rolled sum
+         * last Modified: 2/4/2024 6:21pm By Kenny Rapp
+         */
+        private int ParseAndRoll(string input)
+        {
+            string[] parts = input.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            int totalResult = 0;
+
+            foreach (var part in parts)
+            {
+                if (part.Contains("d"))
+                {
+                    string[] dicePart = part.Split('d');
+                    int numberOfDice = int.Parse(dicePart[0]);
+                    int numberOfFaces = int.Parse(dicePart[1]);
+
+                    totalResult += RollDice(numberOfDice, numberOfFaces);
+                }
+                else
+                {
+                    int modifier = int.Parse(part);
+                    totalResult += modifier;
+                }
+            }
+
+            return totalResult;
+        }
+    }
+
+}
--- DungeonSidekickMAUI/LandingPage.xaml.cs
+++ DungeonSidekickMAUI/LandingPage.xaml.cs
@@ -1,14 +1,13 @@
 namespace DungeonSidekickMAUI;
-
 public partial class LandingPage : ContentPage
 {
     CharacterSheet currentcharacterSheet;
-    static Random random = new Random();
+    DiceRoll diceroller;
     public LandingPage(CharacterSheet passedCharacterSheet)
 	{
         currentcharacterSheet = passedCharacterSheet;
 		InitializeComponent();
-
+        diceroller = new DiceRoll();
         if (currentcharacterSheet != null ) 
         {
             LoadCharacterSheetPage(currentcharacterSheet);
@@ -38,71 +37,22 @@ public partial class LandingPage : ContentPage
         Wis_Mod.Text = wismod.ToString();
         Char_Mod.Text = charmod.ToString();
     }
-    /*
-     * Function: RollDice
-     * Author: Kenny Rapp
-     * Purpose: to roll dice and return the rolled sum
-     * last Modified: 2/4/2024 6:21pm By Kenny Rapp
-     */
-    public int RollDice(int numberOfDice, int numberOfFaces)
-    {
-        int totalSum = 0;
 
-        for (int i = 0; i < numberOfDice; i++)
-        {
-            int rollResult = random.Next(1, numberOfFaces + 1);
-            totalSum += rollResult;
-        }
-
-        return totalSum;
-    }
-    /*
-     * Function: ParseAndRoll
-     * Author: Kenny Rapp
-     * Purpose: to parse the input string and return the rolled sum
-     * last Modified: 2/4/2024 6:21pm By Kenny Rapp
-     */
-    public int ParseAndRoll(string input)
-    {
-        try
-        {
-            string[] parts = input.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
-
-            int totalResult = 0;
-
-            foreach (var part in parts)
-            {
-                if (part.Contains("d"))
-                {
-                    string[] dicePart = part.Split('d');
-                    int numberOfDice = int.Parse(dicePart[0]);
-                    int numberOfFaces = int.Parse(dicePart[1]);
-
-                    totalResult += RollDice(numberOfDice, numberOfFaces);
-                }
-                else
-                {
-                    int modifier = int.Parse(part);
-                    totalResult += modifier;
-                }
-            }
-
-            return totalResult;
-        }
-        catch (Exception ex)
-        {
-            DisplayAlert("Error", $"Invalid input: {ex.Message}", "OK");
-            return 0; // or any default value
-        }
-    }
     /*
      * Function: RollButtonClicked
      * Author: Kenny Rapp
      * Purpose: to call the ParseAndRoll function and set its text to the rolled sum
      * last Modified: 2/4/2024 6:21pm By Kenny Rapp
      */
-    private void RollButtonClicked(object sender, EventArgs e) 
+    private void RollButtonClicked(object sender, EventArgs e)
     {
-        rollbutton.Text = ParseAndRoll(inputentry.Text).ToString();
+        try
+        {
+            rollbutton.Text = diceroller.Roll(inputentry.Text).ToString();
+        }
+        catch (Exception ex) 
+        {
+            DisplayAlert("invalid input(s)", "Please fix your input string", "OK");
+        }
     }
 }
