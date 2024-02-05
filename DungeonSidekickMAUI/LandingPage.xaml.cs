5801938b44cfd8be668f0b47fb4b9ed893f1f548
Wolfl <Wolfl@MY3RDPC>
2024-02-04 18:30:57 -0800

    made a roll function that parses a string input

--- DungeonSidekickMAUI/LandingPage.xaml.cs
+++ DungeonSidekickMAUI/LandingPage.xaml.cs
@@ -3,6 +3,7 @@ namespace DungeonSidekickMAUI;
 public partial class LandingPage : ContentPage
 {
     CharacterSheet currentcharacterSheet;
+    static Random random = new Random();
     public LandingPage(CharacterSheet passedCharacterSheet)
 	{
         currentcharacterSheet = passedCharacterSheet;
@@ -36,5 +37,62 @@ public partial class LandingPage : ContentPage
         Int_Mod.Text = intmod.ToString();
         Wis_Mod.Text = wismod.ToString();
         Char_Mod.Text = charmod.ToString();
+    }
+    /*
+     * Function: RollDice
+     * Author: Kenny Rapp
+     * Purpose: to roll dice and return the rolled sum
+     * last Modified: 2/4/2024 6:21pm By Kenny Rapp
+     */
+    public int RollDice(int numberOfDice, int numberOfFaces)
+    {
+        int totalSum = 0;
+
+        for (int i = 0; i < numberOfDice; i++)
+        {
+            int rollResult = random.Next(1, numberOfFaces + 1);
+            totalSum += rollResult;
+        }
+
+        return totalSum;
+    }
+    /*
+     * Function: ParseAndRoll
+     * Author: Kenny Rapp
+     * Purpose: to parse the input string and return the rolled sum
+     * last Modified: 2/4/2024 6:21pm By Kenny Rapp
+     */
+    public int ParseAndRoll(string input)
+    {
+        string[] parts = input.Split(new char[] { 'd', '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+        if (parts.Length < 2 || parts.Length > 3)
+        {
+            DisplayAlert("input string does not conform with expected input", "The expected input form is XdY [+ Z]", "OK");
+
+        }
+
+        int numberOfDice = int.Parse(parts[0]);
+        int numberOfFaces = int.Parse(parts[1]);
+
+        int modifier = 0;
+        if (parts.Length == 3)
+        {
+            modifier = int.Parse(parts[2]);
+        }
+
+        int diceRollResult = RollDice(numberOfDice, numberOfFaces);
+
+        return diceRollResult + modifier;
+    }
+    /*
+     * Function: RollButtonClicked
+     * Author: Kenny Rapp
+     * Purpose: to call the ParseAndRoll function and set its text to the rolled sum
+     * last Modified: 2/4/2024 6:21pm By Kenny Rapp
+     */
+    private void RollButtonClicked(object sender, EventArgs e) 
+    {
+        rollbutton.Text = ParseAndRoll(inputentry.Text).ToString();
     }
 }
