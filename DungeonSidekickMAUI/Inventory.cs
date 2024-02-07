88b7d19d70ef2757a1a71f74f1f164265ab20f29
Thomas-Hewitt <sirkenmore@gmail.com>
2024-02-07 11:42:34 -0800

    Created logic for creating an Inventory if the ID wasn't found.

--- DungeonSidekickMAUI/Inventory.cs
+++ DungeonSidekickMAUI/Inventory.cs
@@ -12,16 +12,17 @@ namespace DungeonSidekickMAUI
      * Class: Inventory
      * Author: Thomas Hewitt
      * Purpose: Handling the intermediate tables for the items, weapons, and equipment tables.
-     * last Modified: 2/5/2024 by Thomas Hewitt
+     * last Modified: 2/7/2024 by Thomas Hewitt
      */
     public class Inventory
     {
         // Making the string a private member so I don't have to keep instantiating it.
         private string connectionString = "server=satou.cset.oit.edu, 5433; database=harrow; UID=harrow; password=5HuHsW&BYmiF*6; TrustServerCertificate=True; Encrypt=False;";
-        public Inventory(int CharacterID) // Construct the Inventory using the ID of the character it belongs to.
+
+        public Inventory(int CharacterID) // Construct the Inventory using the ID of the character it belongs to. Handles the DB nonsense as well.
         {
             string query = "SELECT * FROM dbo.Inventory" +
-                " WHERE InventoryID = @IID";
+                " WHERE InventoryID = @IID;";
             try
             {
                 using (SqlConnection conn = new SqlConnection(connectionString))
@@ -32,15 +33,26 @@ namespace DungeonSidekickMAUI
                         using (SqlCommand cmd = conn.CreateCommand())
                         {
                             cmd.CommandText = query;
-                            cmd.Parameters.AddWithValue("@IID", IID);
-                            int exists = (int)cmd.ExecuteScalar(); // This will check to see if this inventory exists in the database. Returns null if there is no inventory attached to this character.
+                            cmd.Parameters.AddWithValue("@IID", InventoryID);
+                            int exists = (int)cmd.ExecuteScalar(); // This will check to see if this inventory exists in the database. Returns null if there is no inventory was found.
                             if(exists > 0)
                             {
                                 // Grab the information from the DB, inventory exists.
                             }
                             else
                             {
-                                // Create a new inventory in the DB, inventory doesn't exist.
+                                // Create a new inventory in the DB, inventory doesn't exist. Then, assign the ID to the member variable.
+
+                                string newQuery = "INSERT INTO dbo.Inventory DEFAULT VALUES;"; // Handy trick, since Inventory only contains a PK, DEFAULT VALUES just increments the ID.
+                                cmd.CommandText = newQuery;
+                                cmd.ExecuteNonQuery();
+
+                                newQuery = "SELECT InventoryID=IDENT_CURRENT('dbo.Inventory');"; // This will grab the latest InventoryID that was created.
+                                cmd.CommandText = newQuery;
+                                using (SqlDataReader reader = cmd.ExecuteReader())
+                                {
+                                    InventoryID = reader.GetInt32(0); // Assigns the freshly generated ID to this inventory for later use.
+                                }
                             }
                         }
                     }
@@ -52,34 +64,19 @@ namespace DungeonSidekickMAUI
             }
             // Populate the class with data from the database.
         }
-
-        public required int IID { get; set; } // Stores the ID of the current Inventory.
-
-        //public required int WInterID { get; set; } // Stores the ID of the WInter table.
-
-        //public required int EInterID { get; set; } // Stores the ID of the EInter table.
-
-        public required int IInterID { get; set; } // Stores the ID of the IInter table.
-
-        //public void GetWInterStuff(int IID) // Query the database and present the data from the WInter table. Shows your weapons.
-        //{
-        //    string query = "SELECT * FROM dbo.WInter" +
-        //        " WHERE InventoryID = @IID";
-        //    // Do something with WInter.
-        //}
-
-        //public void GetEInterStuff(int IID) // Query the database and present the data from the EInter table. Shows your equipment.
-        //{
-        //    string query = "SELECT * FROM dbo.EInter" +
-        //        " WHERE InventoryID = @IID";
-        //    // Do something with EInter.
-        //}
-
         public void GetIInterStuff(int IID) // Query the database and present the data from the IInter table. Shows your items + quantities.
         {
             string query = "SELECT * FROM dbo.IInter" +
                 " WHERE InventoryID = @IID";
             // Do something with IInter.
         }
+
+        public void UpdateDB()
+        {
+            // Mass updates the DB with any changes made to this inventory.
+        }
+
+        public required int InventoryID { get; set; } // Stores the ID of the current Inventory.
+
     }
 }
