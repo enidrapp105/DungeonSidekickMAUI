62700ab88874a1a7d63f4de12f63b50bb821bc1b
Jonraff37 <turtlkitsune@hotmail.com>
2024-02-15 07:42:33 -0800

    Got proof of concept working on settings page with examples of adding to sectioner.

--- DungeonSidekickMAUI/Settings_Page.xaml.cs
+++ DungeonSidekickMAUI/Settings_Page.xaml.cs
@@ -2,9 +2,25 @@ namespace DungeonSidekickMAUI;
 
 public partial class Settings_Page : ContentPage
 {
-	public Settings_Page()
-	{
-		InitializeComponent();
+    public Settings_Page()
+    {
+        var customResources = Application.Current.Resources.MergedDictionaries.ToArray()[2];
+        var testSection = (Frame)customResources["PageSection"];
+        var stack = (StackLayout)testSection.Content;
+        var testLabel = new Label
+        {
+            Text = "Here is an example of the code in use.",
+            TextColor = Color.FromRgb(0, 0, 0)
+        };
+        stack.Children.Add(testLabel);
+        var testLabel2 = new Label
+        {
+            Text = "Here is another example of the code in use.",
+            TextColor = Color.FromRgb(0, 0, 0)
+        };
+        stack.Children.Add(testLabel2);
+
+        InitializeComponent();
     }
     private void MainPage(object sender, EventArgs e)
     {
