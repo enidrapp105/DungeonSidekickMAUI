71fedb6b28fd4451cdf2e1d695ac1e3a57044b80
Thomas-Hewitt <sirkenmore@gmail.com>
2024-03-31 13:54:54 -0700

    Getting this branch caught up with my new connection string stuff, and testing to see if the .gitignore works.

---
+++ DungeonSidekickMAUI/Connection.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonSidekickMAUI
+{
+    public class Connection
+    {
+        private static Connection? connection;
+
+        // Public property to get the singleton instance.
+        public static Connection connectionSingleton
+        {
+            get { return connection ?? (connection = new Connection()); }
+        }
+
+        // Private constructor to prevent others from making a new one.
+        private Connection()
+        {
+            // Have to get the key and iv from *somewhere.*
+
+            string encryptionKey = EncryptionGrabber.GetEncryptionKey();
+            string encryptionIV = EncryptionGrabber.GetEncryptionIV();
+            connectionString = Encryption.Encrypt(EncryptionGrabber.GetConnectionString(), encryptionKey, encryptionIV);
+        }
+
+        // The actual reason this class was made.
+        public string? connectionString;
+        protected string? encryptionKey;
+        protected string? encryptionIV;
+    }
+}
---
+++ DungeonSidekickMAUI/EncryptionGrabber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DungeonSidekickMAUI
+{
+    public class EncryptionGrabber
+    {
+        private static readonly IConfiguration Configuration;
+
+        static EncryptionGrabber()
+        {
+            var basePath = AppContext.BaseDirectory; // This allows us to find the file without hardcoding a path in.
+            var builder = new ConfigurationBuilder().SetBasePath(basePath).AddJsonFile("secrets.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        public static string GetConnectionString()
+        {
+            return Configuration[$"ConnectionString:string"];
+        }
+
+        public static string GetEncryptionKey()
+        {
+            return Configuration["Encryption:Key"];
+        }
+
+        public static string GetEncryptionIV()
+        {
+            return Configuration["Encryption:IV"];
+        }
+    }
+}
