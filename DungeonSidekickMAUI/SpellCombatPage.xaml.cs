634c726588bb111b86c448ff2dae8a8cec262fc2
Sapph1reGalaxy <126834268+Sapph1reGalaxy@users.noreply.github.com>
2024-04-14 15:47:17 -0700

    Beginning the process of linking combat with spell damages

---
+++ DungeonSidekickMAUI/SpellCombatPage.xaml.cs
@@ -0,0 +1,94 @@
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+namespace DungeonSidekickMAUI;
+
+public partial class SpellCombatPage : ContentPage
+{
+    private Inventory inv;
+    private Monster currentMonster;
+    private int selectedWeaponId;
+    public SpellCombatPage()
+    {
+        InitializeComponent();
+        PullSpells();
+    }
+
+    void PullSpells()
+    {
+        Color PrimaryColor = (Color)Microsoft.Maui.Controls.Application.Current.Resources["PrimaryColor"];
+        Color SecondaryColor = (Color)Microsoft.Maui.Controls.Application.Current.Resources["SecondaryColor"];
+        Color TrinaryColor = (Color)Microsoft.Maui.Controls.Application.Current.Resources["TrinaryColor"];
+        Color fontColor = (Color)Microsoft.Maui.Controls.Application.Current.Resources["FontC"];
+
+        inv = new Inventory();
+        inv.PullItems();
+        string connectionString = "server=satou.cset.oit.edu, 5433; database=harrow; UID=harrow; password=5HuHsW&BYmiF*6; TrustServerCertificate=True; Encrypt=False;";
+        foreach (var weapon in inv.Weapons)
+        {
+            string query = "SELECT name FROM dbo.Weapon" +
+            " WHERE WeaponID = @Id;";
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    if (conn.State == System.Data.ConnectionState.Open)
+                    {
+                        using (SqlCommand cmd = conn.CreateCommand())
+                        {
+                            cmd.CommandText = query;
+
+                            // grabs ID from weapon list
+                            cmd.Parameters.AddWithValue("@Id", weapon[0]);
+                            using (SqlDataReader reader = cmd.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    HorizontalStackLayout layout = new HorizontalStackLayout();
+                                    Label weaponLabel = new Label();
+                                    weaponLabel.TextColor = (Color)fontColor;
+
+                                    // grabs the name from the DB and quantity from weapon list
+                                    string name = reader.GetString(0);
+                                    weaponLabel.Text = name + " x" + weapon[1];
+                                    layout.Add(weaponLabel);
+
+                                    // Button that selects the item to be used in combat
+                                    Button select = new Button
+                                    {
+                                        CommandParameter = weapon[0],
+                                        TextColor = fontColor,
+                                        Text = "Select",
+                                        BackgroundColor = TrinaryColor,
+                                    };
+                                    select.Clicked += SelectButton;
+                                    layout.Add(select);
+                                    CombatStack.Add(layout);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception eSql)
+            {
+                DisplayAlert("Error!", eSql.Message, "OK");
+                Debug.WriteLine("Exception: " + eSql.Message);
+            }
+        }
+    }
+    private async void SelectButton(object sender, EventArgs e)
+    {
+        if (sender is Button button && button.CommandParameter is int id)
+        {
+            selectedWeaponId = id;
+            Preferences.Default.Set("SelectedWeapon", id);
+            await DisplayAlert("Selected Weapon", "Successfully selected the item for combat.", "Ok");
+        }
+    }
+    private void SelectMonster(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SelectMonster());
+    }
+}
